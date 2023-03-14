f3e29b6165418cc85bc792313e68123d2029ba76
Luneldevwb <nematt007@gmail.com>
2023-03-14 08:33:16 +0300

    new mod from src

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,9 @@
+// $(document).ready(function () {
+    
+// });
+
+
+
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.menu__close');
@@ -14,4 +20,44 @@ const counters = document.querySelectorAll('.proskills__exp-subtitle');
 
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
+});
+
+//Smooth scroll and page up
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(_href).offset().top + "px"
+    });
+    return false;
+});
+
+AOS.init();
+
+$('form').submit(function (e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+        return;
+    }
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+
+    }).done(function () {
+        $(this).find("input").val("");
+        $('.contacts__descr').fadeOut();
+        // $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
+
 });
